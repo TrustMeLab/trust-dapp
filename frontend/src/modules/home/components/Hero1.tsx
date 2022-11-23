120f9a897a981a7ddb0c986b12aa582c042c4b46
Nugahh <nugah.dev@gmail.com>
2022-11-23 19:20:42 +0100

    Hero1 sample

--- frontend/src/modules/home/components/Hero1.tsx
+++ frontend/src/modules/home/components/Hero1.tsx
@@ -1,7 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import Image from 'mui-image';
 
 export default function Hero1 () {
   return (
-    <Box sx={{ bgcolor: 'black' }}>Hero 1</Box>
+    <Box sx={{
+			height: '28em',
+			boxShadow: 3,
+			display: 'flex',
+			justifyContent: 'center'
+		}}>
+		<Box sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '50%'
+			}}>
+			<Box sx={{mb: '1em', fontSize: '3em'}}>Comment fait Julien pour ne pas se faire report à 42 ?</Box>
+            <Box sx={{mb: '0.4em'}}>L O G O</Box>
+			<Box sx={{mb: '0.4em', fontSize: '1.5em'}}>Outil de gestion de location</Box>
+			<Box sx={{
+					mb: '1em',
+					fontSize: '1.2em',
+					textAlign: 'center'
+			}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</Box>
+			<Box sx={{
+				display: 'flex'
+			}}>
+				<Box sx={{mr: '6em'}}><Button variant="outlined" size="large">Tenant</Button></Box>
+				<Box><Button variant="outlined" size="large">Owner</Button></Box>
+			</Box>
+		</Box>
+	</Box>
   )
 }
+
