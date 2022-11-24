60fd835fbe6a031b9bb8eb6c6c76b4f67893585c
bangtam1994 <bangtam1994@hotmail.com>
2022-11-24 03:07:26 +0100

    add ipfs client for uri review

---
+++ frontend/src/repositories/services/ipfsUtils.ts
@@ -0,0 +1,23 @@
+import { create, IPFSHTTPClient } from "ipfs-http-client";
+
+const postToIPFS = async (data: any): Promise<string> => {
+  let ipfs: IPFSHTTPClient | undefined;
+  let path = "";
+  try {
+    const authorization =
+      "Basic " + btoa(process.env.INFURA_ID + ":" + process.env.INFURA_SECRET);
+    ipfs = create({
+      url: "https://infura-ipfs.io:5001/api/v0",
+      headers: {
+        authorization,
+      },
+    });
+    const result = await (ipfs as IPFSHTTPClient).add(data);
+    path = "https://infura-ipfs.io/ipfs/$%7Bresult.path%7D%60";
+  } catch (error) {
+    console.error("IPFS error ", error);
+  }
+  return path;
+};
+
+export default postToIPFS;
