b28cbcaac75054d6933a989e263b631d323811c7
Quentin D.C <quent043@hotmail.com>
2022-11-25 00:37:48 +0000

    WIP paymennt

---
+++ frontend/src/commons/components/SmallTenantRentCard.tsx
@@ -0,0 +1,38 @@
+import {Box, Button, Card, Typography} from "@mui/material";
+import React from "react";
+import {useTheme} from "@mui/material/styles";
+
+interface SmallTenantCardProps {
+  period: string;
+  rentInfos: string | number;
+  handleClick: () => void;
+}
+
+export const SmallTenantRentCard = ({
+  period, //calculated
+  rentInfos,
+  handleClick,
+  }: SmallTenantCardProps) => {
+  return (<Card
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      borderRadius: "10px",
+      padding: "8px",
+      cursor: "pointer",
+      width: "240px",
+      height: "160px",
+      justifyContent: "space-around",
+      alignItems: "center",
+      "&:hover": {
+        boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px;",
+      },
+    }}
+    onClick={handleClick}
+  >
+    <Typography variant="h6" sx={{fontWeight: "light"}}>
+      {period}
+    </Typography>
+    <Typography variant="h5">{rentInfos}</Typography>
+  </Card>);
+};
