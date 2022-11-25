781a3b8e7bdea5df1e04731238400e478b43a860
bangtam1994 <bangtam1994@hotmail.com>
2022-11-25 02:39:32 +0100

    edit size logo partners

--- frontend/src/modules/home/components/Hero4.tsx
+++ frontend/src/modules/home/components/Hero4.tsx
@@ -1,60 +1,64 @@
-import { Box, Button } from "@mui/material";
-import Image from 'mui-image';
+import { Box } from "@mui/material";
 
-export default function Hero4 () {
+export default function Hero4() {
   return (
-    <Box sx={{
-			mt: '3em', // a voir selon préférences
-			height: '28em',
-			bgcolor: '#FAFAFC',
-			display: 'flex',
-			alignItems: 'center',
-			flexDirection: 'column'
-		}}>
-		<Box sx={{mt: '1.5em', mb: '1.6em', fontSize: '3em'}}>Powered by</Box>
-		<Box sx={{
-			display: 'flex',
-			justifyContent: 'space-around',
-			width: '80%'}}>
-			<Box
-				component="img"
-				sx={{
-				padding: "10px",
-				height: "14%",
-				}}
-				alt="Your logo."
-				src={"../public/starton.png"}
-			/>
-			<Box
-				component="img"
-				sx={{
-				mt: '-1.5em',
-				padding: "10px",
-				height: "25%",
-				}}
-				alt="Your logo."
-				src={"../public/iExec.png"}
-			/>
-			<Box
-				component="img"
-				sx={{
-				padding: "10px",
-				height: "15%",
-				}}
-				alt="Your logo."
-				src={"../public/nodereal.png"}
-			/>
-			<Box
-				component="img"
-				sx={{
-				padding: "10px",
-				height: "15%",
-				}}
-				alt="Your logo."
-				src={"../public/ledger.png"}
-			/>
-		</Box>
-	</Box>
-  )
+    <Box
+      sx={{
+        mt: "3em", // a voir selon préférences
+        height: "28em",
+        bgcolor: "#FAFAFC",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Box sx={{ mt: "1.5em", mb: "1.6em", fontSize: "3em" }}>Powered by</Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-around",
+          width: "80%",
+          height: "100%",
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            padding: "10px",
+            height: "14%",
+          }}
+          alt="starton"
+          src={"/starton.png"}
+        />
+        <Box
+          component="img"
+          sx={{
+            mt: "-1.5em",
+            padding: "10px",
+            height: "25%",
+          }}
+          alt="iExec"
+          src={"/iExec.png"}
+        />
+        <Box
+          component="img"
+          sx={{
+            padding: "10px",
+            height: "15%",
+          }}
+          alt="nodereal"
+          src={"/nodereal.png"}
+        />
+        <Box
+          component="img"
+          sx={{
+            padding: "10px",
+            height: "15%",
+          }}
+          alt="ledger"
+          src={"/ledger.png"}
+        />
+      </Box>
+    </Box>
+  );
 }
-
--- frontend/src/modules/home/components/Homepage.tsx
+++ frontend/src/modules/home/components/Homepage.tsx
@@ -1,19 +1,18 @@
-import {AppBar, Avatar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography} from "@mui/material";
-import { Fragment } from 'react'
+import { Fragment } from "react";
 import HomepageHeader from "./Header";
 import Hero1 from "./Hero1";
 import Hero2 from "./Hero2";
 import Hero3 from "./Hero3";
 import Hero4 from "./Hero4";
 
-export default function Homepage () {
+export default function Homepage() {
   return (
     <Fragment>
       <HomepageHeader />
-      <Hero1/>
-      <Hero2/>
-      <Hero3/>
-      <Hero4/>
+      <Hero1 />
+      <Hero2 />
+      <Hero3 />
+      <Hero4 />
     </Fragment>
-  )
+  );
 }
